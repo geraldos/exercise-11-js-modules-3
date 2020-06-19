5bc605e0c8ec4326d71ac33451931338f792cdbd
geraldos <sepdwijaya18@gmail.com>
2020-06-19 16:03:58 +0700

    update

--- asset/script/function.js
+++ asset/script/function.js
@@ -1,14 +1,11 @@
 function transformText(str) {
-    let strArr = str.split("")
-    let doneStr = strArr.map((characterr) => {
-        if (/[a-z]/.test(characterr)) {
-            characterr = characterr.toUpperCase()
-        } else {
-            characterr = characterr.toLowerCase()
-        }
-        return characterr
-    })
-    return doneStr.join("");
+    let regexTest = /[A-Z]/g;
+
+    if (str.match(regexTest)) {
+        return str.toLowerCase();
+    } else {
+        return str.toUpperCase();
+    }
 }
 
 export default transformText
